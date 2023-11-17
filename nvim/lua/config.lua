f92d5c3a16b15bae564a44a46d8cfac870dd3da4
NothingRealm <shafaghi1378@gmail.com>
2023-11-17 02:07:37 -0500

    Added Iron vim

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -292,6 +292,56 @@ require 'nvim-treesitter.configs'.setup {
     },
 }
 
+local iron = require("iron.core")
+
+repl_open_cmd = "vertical botright 80 split"
+iron.setup {
+  config = {
+    -- Whether a repl should be discarded or not
+    scratch_repl = true,
+    -- Your repl definitions come here
+    repl_definition = {
+      sh = {
+        -- Can be a table or a function that
+        -- returns a table (see below)
+        command = {"zsh"}
+      }
+    },
+    -- How the repl window will be displayed
+    -- See below for more information
+    repl_open_cmd = repl_open_cmd,
+  },
+  -- Iron doesn't set keymaps by default anymore.
+  -- You can set them here or manually add keymaps to the functions in iron.core
+  keymaps = {
+    send_motion = "<space>sc",
+    visual_send = "<space>sc",
+    send_file = "<space>sf",
+    send_line = "<space>sl",
+    send_until_cursor = "<space>su",
+    send_mark = "<space>sm",
+    mark_motion = "<space>mc",
+    mark_visual = "<space>mc",
+    remove_mark = "<space>md",
+    cr = "<space>s<cr>",
+    interrupt = "<space>s<space>",
+    exit = "<space>sq",
+    clear = "<space>cl",
+  },
+  -- If the highlight is on, you can change how it looks
+  -- For the available options, check nvim_set_hl
+  highlight = {
+    italic = true
+  },
+  ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+}
+
+vim.keymap.set('n', '<leader>rs', '<cmd>IronRepl<cr>')
+vim.keymap.set('n', '<leader>rr', '<cmd>IronRestart<cr>')
+vim.keymap.set('n', '<leader>rf', '<cmd>IronFocus<cr>')
+vim.keymap.set('n', '<leader>rh', '<cmd>IronHide<cr>')
+
+
 --require('copilot').setup({
 --  panel = {
 --    enabled = true,
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,5 +1,6 @@
 vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
+    use {'Vigemus/iron.nvim'}
     use 'wbthomason/packer.nvim'
     use 'simrat39/rust-tools.nvim'
     -- Debugging
